727abcdd788de3a6a3301b0346b557416090ce20
Eduardo Ramírez <eddysrg@outlook.com>
2024-04-09 19:04:25 -0600

    Se crea la pagina de administracion de bienes y se implementa la base de datos para poder eliminar y actualizar las propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,50 @@
 <?php 
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+
+
+//Importar la conexion
+require "../includes/config/database.php";
+$db = conectarDB();
+
+//Escribir el query
+$query = "SELECT * FROM propiedades";
+
+//Consultar la DB
+$resultadoConsulta = $db->query($query);
+
+//Muestra mensaje condicional
+$resultado = $_GET["resultado"] ?? null;
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    $id = $_POST["id"];
+    $id = filter_var($id ,FILTER_VALIDATE_INT);
+
+    if($id) {
+
+        //Eliminar el archivo
+        $query = "SELECT imagen FROM propiedades WHERE id = ${id}";
+
+        $resultado = $db->query($query);
+        $propiedad = $resultado->fetch_assoc();
+
+        unlink("../imagenes/" . $propiedad["imagen"]);
+
+
+        //Eliminar la propiedad
+        $query = "DELETE FROM propiedades WHERE id = ${id}";
+
+        $resultado = $db->query($query);
+
+        if($resultado) {
+            header("location: /admin?resultado=3");
+        }
+    }
+}
+
+//Incluye un template
 require '../includes/funciones.php';
 incluirTemplate('header');
 ?>
@@ -6,8 +52,53 @@ incluirTemplate('header');
 <main class="contenedor seccion">
         <h1>Administrador de Bienes Raices</h1>
 
+        <?php if(intval($resultado) === 1) : ?>
+            <p class="alerta exito">Anuncio Creado Correctamente</p>
+        <?php elseif(intval($resultado) === 2) : ?>
+            <p class="alerta exito">Anuncio Actualizado Correctamente</p>
+        <?php elseif(intval($resultado) === 3) : ?>
+            <p class="alerta exito">Anuncio Eliminado Correctamente</p>
+        <?php endif; ?>
+
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <!-- Mostrar los resultados -->
+            <tbody>
+                <?php while($propiedad = $resultadoConsulta->fetch_assoc()): ?>
+                <tr>
+                    <td><?php echo $propiedad["id"]; ?></td>
+                    <td><?php echo $propiedad["titulo"]; ?></td>
+                    <td><img src="/imagenes/<?php echo $propiedad["imagen"]; ?>" class="imagen-tabla"></td>
+                    <td>$<?php echo $propiedad["precio"]; ?></td>
+                    <td>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad["id"]; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
 
 </main>
 
-<?php incluirTemplate("footer") ?>
+<?php 
+
+//Cerrar la conexión
+$db->close();
+
+incluirTemplate("footer") 
+?>
--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -26,9 +26,13 @@
     // Ejecutar el código después de que el usuario envia el formulario
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        //echo "<pre>";
-        //var_dump($_POST);
-        //echo "</pre>";
+        echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+
+        echo "<pre>";
+        var_dump($_FILES);
+        echo "</pre>";
 
         $titulo = mysqli_real_escape_string($db, $_POST["titulo"]);
         $precio = mysqli_real_escape_string($db, $_POST["precio"]);
@@ -100,15 +104,14 @@
             move_uploaded_file($imagen["tmp_name"], $carpetaImagenes . $nombreImagen);
 
             //Insertar en la base de datos
-            $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedores_id) VALUES ('$titulo', '$precio', $imagen, '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedores_id')";
+            $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedores_id) VALUES ('$titulo', '$precio', '$nombreImagen', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedores_id')";
 
 
             $resultado = $db->query($query);
 
             if($resultado) {
                 //Redireccionar al usuario
-
-                header("Location: /admin");
+                header("Location: /admin?resultado=1");
             }
         }
     }
